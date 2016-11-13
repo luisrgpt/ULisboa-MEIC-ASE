a6c07e4b52d7c47f3be9a9b40d9a39615da23d25
Joao Neves <joaoneves792@gmail.com>
2016-11-13 12:00:29 +0000

    Code cleanup for lab3

--- Lab3/Lab3_master/Lab3_master.ino
+++ Lab3/Lab3_master/Lab3_master.ino
@@ -15,18 +15,8 @@ const int potPin = A3;  // Analog input pin that the potentiometer is attached t
 const int lightPin = A1;  // Analog input pin that the light sensor is attached to
 const int tempPin = A0; // Analog input pin that the temperature sensor is attached to
 
-const int potLed = 4; // Normal pin the pot led is attached to
-const int lightLed = 3; //PWM pin for the light led
-const int tempLed = 2; // Normal pin the tempreature led is attached to
-
-
 void setup() {
-  // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
   Wire.begin();
-  pinMode(potLed, OUTPUT);
-  pinMode(lightLed, OUTPUT);
-  pinMode(tempLed, OUTPUT);
 }
 
 byte handlePot(){
@@ -42,18 +32,14 @@ byte handlePot(){
 
   if(potBlinkTime <= 0){
     potBlinkOn = !potBlinkOn;
-    //digitalWrite(potLed, (potBlinkOn)?HIGH:LOW);
     potBlinkTime = map(analogRead(potPin), 0, 1023, 200, 2000);
-
   }
   return (byte)potBlinkOn;
-
 }
 
 byte handleLight(){
   int lightValue = analogRead(lightPin);
   int lightLedIntensity = map(lightValue, 0, 1023, 255, 0);
-  //analogWrite(lightLed, lightLedIntensity);
   return lightLedIntensity;
 }
 
@@ -62,23 +48,15 @@ byte handleTemperature(){
   
   int sensorValue = analogRead(tempPin);
   int temperature = (((sensorValue/1024.0)*5)-0.5)*100;
-  //int temperature = (sensorValue*0.1096)-31.34; //This transformation is only for joao's temp sensor
 
-  //digitalWrite(temperature > triggerTemperature)?HIGH:LOW);
   return (temperature > triggerTemperature)?1:0;
 }
 
 void loop() {
-  byte pot = handlePot();
-  delay(2);
-  byte light = handleLight();
-  delay(2);
-  byte temp = handleTemperature();
-  delay(2);
-
   Wire.beginTransmission(8);
-  Wire.write(pot);
-  Wire.write(light);
-  Wire.write(temp);
+  Wire.write(handlePot());
+  Wire.write(handleLight());
+  Wire.write(handleTemperature());
   Wire.endTransmission();
+
 }
--- Lab3/Lab3_slave/Lab3_slave.ino
+++ Lab3/Lab3_slave/Lab3_slave.ino
@@ -13,7 +13,15 @@ const int potLed = 4; // Normal pin the pot led is attached to
 const int lightLed = 3; //PWM pin for the light led
 const int tempLed = 2; // Normal pin the tempreature led is attached to
 
-void callbackFunction(int i);
+void callbackFunction(int i){
+  byte potBlinkOn = Wire.read();
+  byte lightLedIntensity = Wire.read();
+  byte tempOn = Wire.read();
+  
+  digitalWrite(potLed, (potBlinkOn)?HIGH:LOW);
+  analogWrite(lightLed, lightLedIntensity);
+  digitalWrite(tempLed, tempOn);
+}
 
 void setup() {
   // initialize serial communications at 9600 bps:
@@ -24,16 +32,6 @@ void setup() {
   pinMode(potLed, OUTPUT);
   pinMode(lightLed, OUTPUT);
   pinMode(tempLed, OUTPUT);
-}
-
-void callbackFunction(int i){
-  byte potBlinkOn = Wire.read();
-  byte lightLedIntensity = Wire.read();
-  byte tempOn = Wire.read();
-  
-  digitalWrite(potLed, (potBlinkOn)?HIGH:LOW);
-  analogWrite(lightLed, lightLedIntensity);
-  digitalWrite(tempLed, tempOn);
 }
 
 void loop() {
