300b287774c4380bc4969475cfe7ea28ac8e1036
Joao Neves <joaoneves792@gmail.com>
2016-11-26 19:37:37 +0000

    Added code to handle the potentiometer (and improved other stuff)

--- Project/Controller/Controller.ino
+++ Project/Controller/Controller.ino
@@ -1,42 +1,43 @@
 #include <Wire.h>
 
 //PINs
-#define onOffButtonPin 666
-#define potentiometerPin 666
-#define txrxLedPin 666
+#define ON_OFF_BUTTON_PIN 666
+#define POTENTIOMETER_PIN 666
+#define TX_RX_LED_PIN 666
 
 #define NOISE_RANGE 10
 
 //Adresses
-#define lightsCount 2
-const byte lights[2] = {8, 9};
+#define LIGHTS_COUNT 2
+const byte lights_adress[LIGHTS_COUNT] = {8, 9};
+
+//Cycle durations
+#define MIN_CYCLE 2000
+#define MAX_CYCLE 10000
 
 //API Commands
-enum API {
-  RED = 0,
-  PING = 1,
-  ACK = 2,
-  ON = 3,
-  OFF = 4,
-  GRN = 5,
-  TIME = 6
-};
+#define RED 0
+#define PING 1
+#define ACK 2
+#define ON 3
+#define OFF 4
+#define GRN 5
+#define TIME 6
 
 //COLORS
-enum COLORS{
-  CLR_GREEN = 0,
-  CLR_YELLOW = 1,
-  CLR_RED = 3
-};
+#define CLR_GREEN 0
+#define CLR_YELLOW 1
+#define CLR_RED 3
+
 
 //Status
-byte currentLightColors[lightsCount] = {CLR_YELLOW, CLR_YELLOW};
+byte currentLightColors[LIGHTS_COUNT] = {CLR_YELLOW, CLR_YELLOW};
 
 
 void blinkTxRxLed(){
   static bool ledOn = false;
   ledOn = !ledOn;
-  digitalWrite(txrxLedPin, (ledOn)?HIGH:LOW);
+  digitalWrite(TX_RX_LED_PIN, (ledOn)?HIGH:LOW);
 }
 
 void send(byte address, byte command, int argument){
@@ -53,25 +54,21 @@ void send(byte address, byte command, int argument){
 
 byte slaveAddressToLightId(byte address){
   byte i;
-  for(i=0;i<lightsCount;i++){
-    if(lights[i] == address)
+  for(i=0;i<LIGHTS_COUNT;i++){
+    if(lights_adress[i] == address)
       return i;
   }
   return 0; //In case of failure return the first light to avoid crashes
 }
 
-short handlePotentiometer(){
-  /*TODO*/
-}
-
 void changeLightStatus(byte light, byte color){
-  if(light < lightsCount);
-  currentLightColors[light] = color;
+  if(light < LIGHTS_COUNT)
+    currentLightColors[light] = color;
 }
 
 void stateOff(){
-  for(int i=0; i<lightsCount; i++){
-    send(lights[i], OFF, 0);
+  for(int i=0; i<LIGHTS_COUNT; i++){
+    send(lights_adress[i], OFF, 0);
   }
   delay(1000); //FIXME: Maybe this causes unresponsiveness for the On/Off button!
 }
@@ -80,13 +77,17 @@ void stateOn(){
   /*TODO*/
 
   static int cycleLength = 0;
-   
-  int newCycleLength = handlePotentiometer();
+
+  //Check the potentiometer for changes and if necessary update the cycle on the traffic lights
+  int newCycleLength = map(analogRead(POTENTIOMETER_PIN), 0, 1023, MIN_CYCLE, MAX_CYCLE);
+
+  //Ignore changes < 10ms possibly caused by noise
   if((newCycleLength > cycleLength+NOISE_RANGE) || (newCycleLength < cycleLength-NOISE_RANGE)){
     cycleLength = newCycleLength;
-    for(byte i=0;i<lightsCount;i++)
-      send(lights[i], TIME, cycleLength);
+    for(byte i=0;i<LIGHTS_COUNT;i++)
+      send(lights_adress[i], TIME, cycleLength); //Update the traffic lights
   }
+  
 }
 
 void registerACK(byte sender){
@@ -94,14 +95,14 @@ void registerACK(byte sender){
 }
 
 void checkIncomingMessages(){
-  for(byte i = 0; i<lightsCount; i++){
+  for(byte i = 0; i<LIGHTS_COUNT; i++){
       //Request 3 bytes from each light
       blinkTxRxLed();
-      Wire.requestFrom(lights[i], (byte)3);
+      Wire.requestFrom(lights_adress[i], (byte)3);
       if(Wire.available() >= 3){
         byte data = Wire.read();
         byte sender = Wire.read();
-        Wire.read(); //Read the padding extra byte
+        Wire.read(); //Read extra byte (should be 0)
         
         //Execute the commands appropriatly
         switch(data){
@@ -122,7 +123,7 @@ bool isOn(){
   static bool prevButtonStatus = false;
 
   //Flip current status if button is pressed and its status is different from the previous execution
-  bool buttonStatus = digitalRead(onOffButtonPin);
+  bool buttonStatus = digitalRead(ON_OFF_BUTTON_PIN);
   if(buttonStatus != prevButtonStatus && buttonStatus)
     On = !On;
 
@@ -130,8 +131,8 @@ bool isOn(){
 }
 
 void setup() {
-  pinMode(onOffButtonPin, INPUT);
-  pinMode(potentiometerPin, INPUT);
+  pinMode(ON_OFF_BUTTON_PIN, INPUT);
+  pinMode(POTENTIOMETER_PIN, INPUT);
   Wire.begin();
 
 }
